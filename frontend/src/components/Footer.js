53face4f2cbb217b0b300d721c1b0e7ea508260f
unknown <rajageorge971@gmail.com>
2020-10-14 14:00:25 -0700

    Setup up our backend

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Container} from 'react-bootstrap'
+const Footer = () => {
+    return (
+        <footer>
+            <Container className="text-center py-3">
+                Copyrights &copy; ProShop
+            </Container>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ frontend/src/components/Product.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Card } from 'react-bootstrap'
+import Rating from './Rating'
+import PropTypes from 'prop-types'
+
+
+function Product({ product }) {
+    return (
+        <Card className="my-3 p-3 rounded">
+            <Link to={`/product/${product._id}`}>
+                <Card.Img src={product.image} variant="top" />
+            </Link>
+
+            <Card.Body>
+            <Link to={`/product/${product._id}`}>
+                <Card.Title as="div">
+                    <strong>
+                        {product.name}
+                    </strong> 
+                </Card.Title>
+            </Link>
+            <Card.Text as="div">
+               <Rating value={product.rating} text={`${product.numReviews} reviews` }/>      
+            </Card.Text>
+            <Card.Text as="h3">
+                ${product.price}
+            </Card.Text>
+            </Card.Body>
+        </Card>
+    )
+}
+
+Rating.propTypes={
+    value:PropTypes.number.isRequired,
+    text:PropTypes.string.isRequired,
+    color:PropTypes.string.isRequired,
+}
+
+export default Product
